d1a6f1ce9b9cb7d5dd2a5024291a9c6b159c3d90
Matt Zukowski <matt@zukowski.ca>
2011-02-09 11:27:03 -0500

    took out Rollcall stuff

--- js/s3.test.js
+++ js/s3.test.js
@@ -1,28 +1,5 @@
 $(document).ready(function(){ 
-  $('.listing').each(function() {
-    if ($(this).has('li.groupable').length > 0) {
-      $(this).addClass('clickable')
-    }
-  })
-  
-  $('.groupable').draggable({
-    revert: 'invalid', 
-    helper: 'clone'
-  })
-  
-  $('.group').droppable({
-    hoverClass: 'dropping',
-    drop: function(event, ui) {
-      $(ui.helper).effect('drop')
-    }
-  })
-  
-  $('.listing').click(function() {
-    if ($(this).has('li').length > 0) {
-      $(this).children('.groupables').slideToggle('fast')
-    }
-  })
-  
+
   rollcall = new S3.Rollcall('http://rollcall.proto.encorelab.org')
   token = rollcall.getCurrentToken()
   rollcall.fetchSessionForToken(token, function(data) {
